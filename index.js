a619144ddfd1fe7b6a2531f3d26d524aa56c6e93
Alstocras <alstoperiment@gmail.com>
2025-09-13 10:20:47 +0000

    basic skeleton

---
+++ index.js
@@ -0,0 +1,6 @@
+function solve() {
+        let equation = document.getElementById("equation").value;
+        let ans = document.getElementById("ans");
+        let node = math.parse(equation);
+        ans.textContent = equation + ' = ' + node.evaluate(); 
+    }
